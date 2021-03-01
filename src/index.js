3a5cd6b74d72b92d9b81716a63df29f375150947
Kadynskaya <olgakad9@gmail.com>
2021-03-01 20:10:48 +0200

    init: human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+    let firstNums = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+                    'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+                    'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let secondNums = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let strNum = number.toString();
+
+    if(number === 0) return 'zero';
+
+    if(number > 0 && number < 20) {
+        return firstNums[number];
+    } else if(strNum.length === 2) {
+        return `${secondNums[strNum[0]]} ${firstNums[strNum[1]]}`.trim();
+    } else if(strNum.length === 3) {
+        if(strNum[1] == 0 && strNum[2] == 0) {
+            return `${firstNums[strNum[0]]} hundred`;
+        } else if(strNum[1] <= 1) {
+            return `${firstNums[strNum[0]]} hundred ${firstNums[Number(strNum[1] + strNum[2])]}`;
+        } else if(strNum[1] > 1) {
+            return `${firstNums[strNum[0]]} hundred ${secondNums[strNum[1]]} ${firstNums[strNum[2]]}`.trim();
+        }   
+    }
 }
